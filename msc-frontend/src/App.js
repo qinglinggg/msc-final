5e4e0d5be7da417362d65ba33f628f664a27117d
josur <jsuryadharma9@gmail.com>
2022-01-25 14:50:16 +0700

    Fixing syntax on backend functions. 2

--- msc-frontend/src/App.js
+++ msc-frontend/src/App.js
@@ -15,7 +15,7 @@ import DataVisualization from "./components/Data-visualization";
 import Feedback from "./components/Feedback";
 import Message from "./components/Message";
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'http://msc-backend';
 
 class App extends React.Component {
 
