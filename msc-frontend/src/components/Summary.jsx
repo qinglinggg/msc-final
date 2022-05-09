0892f3dcc1edf20a7acc73030e0f5bf2cb85a902
Jonathan Suryadharma <jsuryadharma@gmail.com>
2022-05-09 13:34:52 +0700

    Added getFormItemResponse, methods for Data Visualizing

--- msc-frontend/src/components/Summary.jsx
+++ msc-frontend/src/components/Summary.jsx
@@ -5,17 +5,17 @@ const Summary = React.forwardRef((props, ref) => {
   let counter = 0;
   return (
     <React.Fragment>
-        {props.formItems_data.map((data) => {
+        {props.data.map((item) => {
           counter += 1;
           return (
             <div className="result-container">
               <div className="question-field">
-                {counter}. {data.question}
+                {counter}. {item.question}
               </div>
               {/* {console.log("graph" + counter)} */}
               <div className="graph-section">
                 <div className="graph">
-                  <Graph Question={data.question} count={counter} />
+                  <Graph Question={item.question} count={counter} />
                 </div>
                 <div className="sub-graph"></div>
               </div>
