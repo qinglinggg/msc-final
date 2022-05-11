acc588ac0d8ba1d703ffcaeba844035d0038722f
Lynnelle <lim.lynnelle@gmail.com>
2022-05-11 14:11:48 +0700

    added isRequired column in backend

--- msc-backend/src/main/resources/db/migration/V3__FormItems.sql
+++ msc-backend/src/main/resources/db/migration/V3__FormItems.sql
@@ -3,6 +3,7 @@ CREATE TABLE FormItems (
     formItemsId VARCHAR(100) NOT NULL,
     itemNumber INT NOT NULL,
     questionContent VARCHAR(255),
-    questionType VARCHAR(50) NOT NULL
+    questionType VARCHAR(50) NOT NULL,
+    isRequired INT NOT NULL
     PRIMARY KEY(formId, formItemsId)
 )
