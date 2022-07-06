869878a3c997f03079966f6915985878b6fd54c6
Jonathan Suryadharma <jsuryadharma@gmail.com>
2022-07-06 16:15:51 +0700

    Solved bugs on Submit Form 500

--- msc-frontend/src/components/Respondent.jsx
+++ msc-frontend/src/components/Respondent.jsx
@@ -91,7 +91,6 @@ function Respondent (props) {
           data: userId_data, 
         }).then((res) => {
           if(res.data && res.data.length > 0){
-            // setFormRespondentId(res.data[0]);
             console.log("formres", res.data);
             setFormRespondentId(res.data.formRespondentId);
             localStorage.setItem("alreadySubmitted", JSON.stringify(res.data.submitDate));
@@ -101,6 +100,7 @@ function Respondent (props) {
               formId: formId,
               userId: userId,
               isTargeted: 0,
+              versionNo: 0
             }
             axios({
               method: "post",
@@ -193,6 +193,10 @@ function Respondent (props) {
       setFormResponse(loadData);
     }
   }, [formRespondentId, answerSelection]);
+
+  useEffect(() => {
+    console.log("formRespondentId: ", formRespondentId);
+  }, [formRespondentId]);
 
   useEffect(() => {
     if(previewMode) return;
@@ -1020,7 +1024,7 @@ function Respondent (props) {
                     );
                   })}
                 </div>
-                <div className="text-indicator"><span id="page-num">{index}</span><span id="max-page">/{formItems.length}</span></div>
+                <div className="text-indicator">{ formItems && index <= formItems.length ? (<React.Fragment><span id="page-num">{index}</span><span id="max-page">/{formItems.length}</span></React.Fragment>) : "Completed"}</div>
               </div>
               {!previewMode ? (
                 <div id="respondent-chat">
