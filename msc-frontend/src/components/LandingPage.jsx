272885405c90ccdffe75835fa9c5d35ead279203
Jonathan Suryadharma <jsuryadharma@gmail.com>
2022-07-06 10:30:02 +0700

    Changes ip configuration on API Backend call

--- msc-frontend/src/components/LandingPage.jsx
+++ msc-frontend/src/components/LandingPage.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React, { Component } from 'react';
 
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "http://10.61.44.90:8080";
 
 class LandingPage extends Component {
     state = {
